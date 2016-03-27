0064cdbfbbec8c27dc5d8708fdd37c414d2e53dc
Rene Reimann <rene@die-pixler.de>
2016-03-27 17:52:50 +0200

    stlay msg

--- prepare-vagrant.sh
+++ prepare-vagrant.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+RED='\033[0;31m'
+GREEN='\033[3;32;40m'
+YELLOW='\033[1;33;40m'
+NC='\033[0m' # No Color
+
 host=$(grep '"host":' composer.json)
 host=${host//'"host":'/''}
 host=${host//'"'/''}
 host=${host//','/''}
-host=${host//'\t'/''}
+host=${host//'   '/''}
 
 hosts="vagrant/config/hosts.list"
 
@@ -23,4 +28,6 @@ Vagrantfile=${Vagrantfile//'192.168.56.101'/${ip}}
 
 rm vagrant/Vagrantfile
 echo "${Vagrantfile}" > vagrant/Vagrantfile
+
+printf "\n  - Configure Vagrant ${GREEN}${host}${NC} (${YELLOW}${ip}${NC})\n    Hosts mapped\n\n"
 
