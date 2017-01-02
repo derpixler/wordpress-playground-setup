878864c18a7b8a28ff8ded6816bccc9a4bb4dec5
Rene Reimann <rene@die-pixler.de>
2017-01-02 20:14:15 +0100

    fix wp-config sync, add db backupp to deploy and rollback

--- deploy.php
+++ deploy.php
@@ -16,21 +16,39 @@ server('preview', 'p366984.mittwaldserver.info')
     ->user('p366984')
     ->identityFile()
     ->set('deploy_path', '/home/www/p366984/html/deployment/preview')
-	->set('stage', 'preview' );
+	->set('base_path', '/home/www/p366984/html/preview')
+	->set('sudo', FALSE)
+	->set('stage', 'preview' )
+	->set('DB_HOST', 'db4686.mydbserver.com' )
+	->set('DB_USERNAME', 'p366984' )
+	->set('DB_PASSWORD', 'usr_p366984_2' )
+	->set('DB_DATABASE', 'Adivimaq%965' );
 
 server('production', 'p366984.mittwaldserver.info')
     ->user('p366984')
     ->identityFile()
     ->set('deploy_path', '/home/www/p366984/html/deployment/public')
-	->set('stage', 'production' );
+	->set('base_path', '/home/www/p366984/html/public')
+	->set('sudo', FALSE)
+	->set('stage', 'production' )
+	->set('DB_HOST', 'db4686.mydbserver.com' )
+	->set('DB_USERNAME', 'p366984' )
+	->set('DB_PASSWORD', 'usr_p366984_1' )
+	->set('DB_DATABASE', 'Adivimaq%965' );
+
 
 server('test', '139.59.135.182')
     ->user('deployer')
     ->identityFile()
     ->set('deploy_path', '/var/www/deployment/')
-	->set('stage', 'test' );
+	->set('base_path', '/var/www/public')
+	->set('sudo', TRUE)
+	->set('stage', 'test' )
+	->set('DB_HOST', 'localhost' )
+	->set('DB_USERNAME', 'wordpress' )
+	->set('DB_PASSWORD', 'IKiu2eiqu1shahghievoo9teidoc5ies' )
+	->set('DB_DATABASE', 'wordpress' );
 
-// Tasks
 
 
 #serverList('servers.yml');
@@ -43,29 +61,103 @@ task('deploy:start_info', function() {
 
 });
 
-#desc('Restart PHP-FPM service');
-#task('php-fpm:restart', function () {
-#    // The user must have rights for restart service
-#    // /etc/sudoers: username ALL=NOPASSWD:/bin/systemctl restart php-fpm.service
-#    run('sudo systemctl restart php-fpm.service');
-#});
-#after('deploy:symlink', 'php-fpm:restart');
+desc('Move wp-config');
+task('deploy:move_wp_config', function() {
+
+    $link = run("readlink {{deploy_path}}/current")->toString();
+    $currentRelease = substr($link, 0, 1) === '/' ? $link : get('deploy_path') . '/' . $link;
+	$wp_config = $currentRelease . '/assets/data/wp-config.dist.php';
+
+	$sudo = get('sudo');
+
+	if(  $sudo === TRUE ){
+		$sudo = 'sudo ';
+	}
+
+	run( $sudo . "cp " . $currentRelease . "/assets/data/wp-config.dist.php " . get('base_path') . "/wp-config.php");
+
+});
+
+desc('Backup DB');
+task('deploy:backup_db', function() {
+
+	$link = run("readlink {{deploy_path}}/current")->toString();
+    $currentRelease = substr($link, 0, 1) === '/' ? $link : get('deploy_path') . '/' . $link;
+
+	$backup_dir = get('deploy_path') . 'db_backup';
+
+	// Create the backup dir if it doesn't exist
+	run( sprintf("if [ ! -d %s ]; then mkdir -p %s; fi", $backup_dir, $backup_dir ) );
+
+    run(sprintf(
+           'mysqldump -h%s -u%s -p%s %s | gzip > %s/%s.sql.gz',
+		   get('DB_HOST'),
+		   get('DB_USERNAME'),
+		   get('DB_PASSWORD'),
+		   get('DB_DATABASE'),
+           $backup_dir,
+           basename( $currentRelease )
+       ));
+
+});
+
 
 desc('Deploy your project');
 task('deploy', [
 	'deploy:start_info',
-    'deploy:prepare',
-    'deploy:lock',
-    'deploy:release',
-    'deploy:update_code',
-    'deploy:shared',
-    'deploy:writable',
-    'deploy:vendors',
-    'deploy:clear_paths',
-    'deploy:symlink',
-    'deploy:unlock',
-    'cleanup',
-    'success'
+    #'deploy:prepare',
+    #'deploy:lock',
+	'deploy:backup_db'
+    #'deploy:release',
+    #'deploy:update_code',
+    #'deploy:shared',
+    #'deploy:writable',
+    #'deploy:vendors',
+    #'deploy:clear_paths',
+    #'deploy:symlink',
+    #'deploy:move_wp_config',
+	#'deploy:unlock',
+    #'cleanup',
+    #'success'
 ]);
 
 after('deploy', 'success');
+
+desc('Backup DB');
+task('rollback', function() {
+
+    $releases = get('releases_list');
+
+    if (isset($releases[1])) {
+        $releaseDir = "{{deploy_path}}/releases/{$releases[1]}";
+
+        // Symlink to old release.
+        run("cd {{deploy_path}} && {{bin/symlink}} $releaseDir current");
+
+		// Remove release
+        run("rm -rf {{deploy_path}}/releases/{$releases[0]}");
+
+		$link = run("readlink {{deploy_path}}/current")->toString();
+	    $currentRelease = substr($link, 0, 1) === '/' ? $link : get('deploy_path') . '/' . $link;
+
+		$backup_dir = get('deploy_path') . 'db_backup';
+
+	    run(sprintf(
+	           'gunzip < %s/%s.sql.gz | mysql -h%s -u%s -p%s %s',
+	           $backup_dir,
+	           basename( $currentRelease ),
+			   get('DB_HOST'),
+			   get('DB_USERNAME'),
+			   get('DB_PASSWORD'),
+			   get('DB_DATABASE')
+	       ));
+
+
+
+        writeln("Rollback to `{$releases[1]}` release was successful.");
+
+    } else {
+        writeln("<comment>No more releases you can revert to.</comment>");
+    }
+
+} );
